8fbd65f354da9a491179da4b87c3b4876e58c405
jcarrenoa <jcarrenoa@uninorte.edu.co>
2026-05-11 21:08:32 -0500

    fix: update CORS middleware to allow all origins

--- app/main.py
+++ app/main.py
@@ -16,8 +16,8 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
     allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],  # importante
     allow_headers=["*"],
 )
