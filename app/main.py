3a93be3da00a0ad74661fe7b9428df6f14e904ac
jcarrenoa <jcarrenoa@uninorte.edu.co>
2026-05-19 22:57:20 -0500

    feat: refactor main.py to include additional routers and implement lifespan context manager

--- app/main.py
+++ app/main.py
@@ -4,15 +4,33 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from contextlib import asynccontextmanager
 import app.api.models
-from app.api.routers import auth, citas, especialidades, horarios, medicos, usuarios
+from app.api.routers import (
+    auth,
+    citas,
+    especialidades,
+    horarios,
+    medicos,
+    usuarios,
+    historiales_clinicos,
+    medicamentos,
+    recetas_medicas,
+    tipos_medicamento,
+)
 from app.database.session import Base, engine
 
-app = FastAPI(title="CareFlow API")
 
 origins = [
     "http://localhost:4200",  # Angular
     "http://127.0.0.1:4200",
 ]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    load_dotenv()
+    Base.metadata.create_all(bind=engine)
+    yield
+
+app = FastAPI(title="CareFlow API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,15 +40,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    load_dotenv()
-    Base.metadata.create_all(bind=engine)
-    yield
-
 app.include_router(usuarios.router)
 app.include_router(auth.router)
 app.include_router(especialidades.router)
 app.include_router(medicos.router)
 app.include_router(citas.router)
 app.include_router(horarios.router)
+app.include_router(historiales_clinicos.router)
+app.include_router(medicamentos.router)
+app.include_router(recetas_medicas.router)
+app.include_router(tipos_medicamento.router)
